914a209ab5d1c23ff979519608421f44e7b79314
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-03-10 20:57:19 +0000

    feat: Adding new client features to fetch nominations using query parameters.

--- client.js
+++ client.js
@@ -1,6 +1,21 @@
 function getNominations() {
     console.log("Getting Nominations:");
-    fetch('http://localhost:8080/getNominations')
+    const baseURL = 'http://localhost:8080/getNominations';
+
+    const inputElement = document.getElementById('year').value;
+
+    const queryParams = {
+        year: inputElement
+    };
+
+    const queryString = Object.keys(queryParams)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
+        .join('&');
+
+    const url = `${baseURL}?${queryString}`;
+    console.log(url);
+
+    fetch(url)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -10,6 +25,7 @@ function getNominations() {
         })
         .then(data => {
             // Handle response data
+            clearNominationsTable();
             buildNominationsTable(data);
             console.log(data);
         })
@@ -21,7 +37,23 @@ function getNominations() {
 
 function getNominees() {
     console.log("Getting Nominees.");
-    fetch('http://localhost:8080/getNominees')
+
+    const baseURL = 'http://localhost:8080/getNominees';
+
+    const inputElement = document.getElementById('year').value;
+
+
+    const queryParams = {
+        year: inputElement
+    };
+
+    const queryString = Object.keys(queryParams)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
+        .join('&');
+
+    const url = `${baseURL}?${queryString}`;
+    console.log(url);
+    fetch(url)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -31,6 +63,7 @@ function getNominees() {
         })
         .then(data => {
             // Handle response data
+            clearNomineesTable();
             buildNomineesTable(data);
             console.log(data);
         })
@@ -70,4 +103,51 @@ function buildNomineesTable(data) {
 
         tableBody.appendChild(row);
     });
+}
+
+function clearOutput() {
+    clearNominationsTable();
+    clearNomineesTable();
+}
+
+function clearNominationsTable() {
+    // Get the table body element
+    const nomineesTableBody = document.getElementById('nomineesBody');
+
+    // Remove all child nodes (rows) from the table body
+    while (nomineesTableBody.firstChild) {
+        nomineesTableBody.removeChild(nomineesTableBody.firstChild);
+    }
+}
+
+function clearNomineesTable() {
+    // Get the table body element
+    const nomineesTableBody = document.getElementById('nomineeBody');
+
+    // Remove all child nodes (rows) from the table body
+    while (nomineesTableBody.firstChild) {
+        nomineesTableBody.removeChild(nomineesTableBody.firstChild);
+    }
+}
+
+function clearInputs() {
+    // Array of input IDs to clear
+    const inputIds = ['year', 'category', 'nominee', 'info', 'nomInfo', 'times'];
+
+    // Loop through each input ID
+    inputIds.forEach(id => {
+        // Get the input element by its ID
+        const inputElement = document.getElementById(id);
+
+        // Clear the value of the input field
+        if (inputElement) {
+            inputElement.value = '';
+        }
+    });
+
+    // Clear the select dropdown value
+    const selectElement = document.getElementById('won');
+    if (selectElement) {
+        selectElement.value = ''; // Set the value to an empty string
+    }
 }
